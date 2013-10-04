95a76809f35ec4855a9ef94b9e3e50a4b7261be6
nacho22martin <nacho22martin@gmail.com>
2013-10-04 11:19:20 -0300

    changes

--- js/contact_form.js
+++ js/contact_form.js
@@ -3,7 +3,7 @@
 */
 jQuery(document).ready(function() {
 	
-    var position = new google.maps.LatLng(-25.25913,-57.565597);    
+    var position = new google.maps.LatLng(-2.569424,-42.739865);    
     $('.map').gmap({'mapTypeId':google.maps.MapTypeId.SATELLITE,'center': position,'zoom': 15, 'disableDefaultUI':true, 'callback': function() {
             var self = this;
             self.addMarker({'position': this.get('map').getCenter() });	
